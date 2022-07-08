42097637be4257bd6c5a4ab94685d3508c966af6
benoti96 <107107035+benoti96@users.noreply.github.com>
2022-07-08 10:27:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+print(len(input('What is your name?\n')))
+# \n puts things on a new line.
+
+
+name = input("What is your name?\n")
+length=len(name)
+print(length)
+
+s = "abcd"
+print(len(s))
+
+print("Hello" + " " + "Ben")
+
+name = input("What is your name\n")
+input("What is" + " " + name + " " + "phone number?\n")
+print(name + " " + "phone number is 079504")
